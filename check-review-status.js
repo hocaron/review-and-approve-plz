afc70f04c0248e987e07d54207cad57cf75921ab
hocaron <kkannu0407@gmail.com>
2023-07-08 01:08:36 +0900

    test: pr 테스트

--- check-review-status.js
+++ check-review-status.js
@@ -1,35 +1,37 @@
 const { IncomingWebhook } = require('@slack/webhook');
 const { Octokit } = require('@octokit/rest');
 
-// 리뷰어의 GitHub 사용자명을 리뷰어 목록에 맞게 변경하세요.
-const REVIEWERS_LIST = ['hocaron'];
-
 // Secrets에서 SLACK_WEBHOOK_URL 값을 가져오거나 직접 입력하세요.
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 
 async function checkReviewStatus() {
     try {
         const octokit = new Octokit();
-        const pullRequests = await octokit.pulls.list({
-            owner: 'hocaron',
-            repo: 'review-and-approve-plz',
-            state: 'open',
+        const { owner, repo, number } = process.env.GITHUB_REPOSITORY.split('/');
+        const pullRequest = await octokit.pulls.get({
+            owner,
+            repo,
+            pull_number: number,
         });
 
-        const pendingReviewPrs = pullRequests.data.filter(pr => {
-            const requestedReviewers = pr.requested_reviewers.map(reviewer => reviewer.login);
-            const missingReviewers = REVIEWERS_LIST.filter(reviewer => !requestedReviewers.includes(reviewer));
-            return missingReviewers.length > 0;
+        const requestedReviewers = pullRequest.data.requested_reviewers.map(reviewer => reviewer.login);
+        const reviews = await octokit.pulls.listReviews({
+            owner,
+            repo,
+            pull_number: number,
         });
 
-        if (pendingReviewPrs.length > 0) {
+        const reviewers = reviews.data.map(review => review.user.login);
+        const missingReviewers = requestedReviewers.filter(reviewer => !reviewers.includes(reviewer));
+
+        if (missingReviewers.length > 0) {
             const slackWebhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
             const message = {
                 text: 'The following pull requests are pending review:',
-                attachments: pendingReviewPrs.map(pr => ({
-                    title: pr.title,
-                    title_link: pr.html_url,
-                })),
+                attachments: [{
+                    title: pullRequest.data.title,
+                    title_link: pullRequest.data.html_url,
+                }],
             };
             await slackWebhook.send(message);
         }
